9205747f6d86f103e2584a6db55ce9664e9d5e86
vladatisenko <ggkalina@yandex.ru>
2024-08-21 18:25:18 +0300

    Add README.md

---
+++ variables.py
@@ -0,0 +1,94 @@
+# сдесь пытался написать о себе
+my_name: str = "Gregory"
+my_girl: str = "Vlada"
+my_friend: str = "Evgen"
+live_color: str = "blue"
+my_hobby: str = "gym"
+my_work: str = "Tester"
+a_laptop: str ="macbook m2"
+year: str = "15.08.2024 год"
+
+
+# просто сделал рандомный перевод слов
+hide: str = "прятать"
+hire: str = "нанимать"
+ship: str = 'корабль'
+shower: str = 'душ'
+sink: str = 'раковина'
+shirt: str = 'штаны'
+meet: str = 'встречать'
+tree: str = 'дерево'
+mild: str = 'мягкий'
+weekday: str = 'будний день'
+horse: str = 'лошадь'
+see: str = 'видеть'
+feel: str = 'чувствовать'
+happy: str = 'счастливый'
+hungry: str = 'голодный'
+see_you: str = 'увидмся'
+life: str = 'жизнь'
+crime: str = 'преступление'
+a_pen: str = 'ручка'
+window: str = 'окно'
+door: str = 'дверь'
+table: str = 'стол'
+rose: str = 'роза'
+sunglasses: str = 'солнцезащитные очки'
+coins: str = 'монеты'
+smartphone: str = 'телефон'
+earphones: str = 'наушники'
+camera: str = 'камера'
+bag: str = 'сумка'
+lipgloss: str = 'блеск для губ'
+charger: str = 'зарядное устройство'
+key: str = 'ключ'
+perfume: str = 'парфюм'
+scarf: str = 'шарф'
+dictionary: str = 'словарь'
+note: str = 'примечание'
+be_late: str = 'увидимся'
+map: str = 'карта'
+purse: str = 'кошелек'
+tablet: str = 'планшет'
+towel: str = 'полотенце'
+batteries: str = 'батарейка'
+hello: str = 'привет'
+
+
+# переменные типа float()
+a = float(1.1)
+b = float(2.8)
+c = float(13.5, 'тринадцать целых, пять десятых')
+point = float(15.753)
+d = float(10.9)
+year = float(2024.08)
+ten = float(10)
+# если я правильно понял, то данная команда делает целое число с плавающей точкой
+# или сохраняет все что стоит после точки.
+
+
+# переменные типы int()
+number: int = 222
+summa: int = 10 + 10 * 13
+number1 = int(100)
+a: int = 1
+b: int = 2
+c: int = 3
+ten: int = 10.8 # получится целое число 10
+phone_namber: int = 89151723261 # почему то кажется что это лучше делать в стоке
+
+# нет фантазии что еще написать, по факту команда int присваивает статус числа или цыфры причем целой
+
+# переменные кортежы tuple()
+numbers = tuple("123456789")
+
+# как я понял можно также команду создавать просто со скобками и через запятую что бы не разделяло по буквам или цыфрам
+new_york_tuple = ("1 января 2025",)
+car_tuple = ("хюндай солярис",)
+adres_tuple = ("московская область, г. Люберцы",)
+work_tuple = ("ddx Fitness 36",)
+
+
+
+
+
